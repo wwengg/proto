75b2f457e200979579f369ce23adb11e8e66f1b0
WangWengang <wwwwangg@163.com>
2021-08-21 11:17:00 +0800

    feat: 若干

--- generate.go
+++ generate.go
@@ -5,5 +5,7 @@
 package proto
 
 //go:generate protoc -I. --go_out=plugins=rpcx:. --go_opt=paths=source_relative pigeon/room.proto
+//go:generate protoc -I. --go_out=plugins=rpcx:. --go_opt=paths=source_relative pigeon/pigeon.proto
+//go:generate protoc -I. --go_out=plugins=rpcx:. --go_opt=paths=source_relative pigeon/protocol.proto
 //go:generate protoc -I. --go_out=plugins=rpcx:. --go_opt=paths=source_relative identity/identity.proto
 //go:generate protoc -I. --go_out=plugins=rpcx:. --go_opt=paths=source_relative common/common.proto
